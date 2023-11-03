5030ddbb6405f5e16436ecc76cceae3669bf47fd
angelashi <angelas2026@student.cis.edu.hk>
2023-11-03 08:28:33 +0800

    Finished Quiz 4.1

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,17 @@
+import java.util.Scanner;
 public class Main {
 
     /** 1. Write a method called numberOfVowels that returns the number of vowels in a string.
      Consider a, e, i, o, u as vowels. **/
-    
+    public static int numberOfVowels(String str){
+        int numVowels = 0;
+        for (int i = 0; i < str.length(); i++){
+            if ((str.charAt(i) == 'a') || (str.charAt(i) == 'e') || (str.charAt(i) == 'i') || (str.charAt(i) == 'o') || (str.charAt(i) == 'u')){
+                numVowels += 1;
+            }
+        }
+        return numVowels;
+    }
 
     /** 2. Write a method called notDivisibleBy235 that finds the number of natural numbers not exceeding N
      and not divisible by any of the numbers [2, 3, 5].
@@ -14,7 +23,15 @@ public class Main {
      4 - divides integer by 2
      5 - divides integer by 5
      Answer: 1 (only one number doesn't divide integer by any of 2, 3, 5) **/
-
+    public static int notDivisibleBy235(int num){
+        int canDivide = 0;
+        for (int i = 1; i <= num; i++){
+            if ((i % 2 == 0) || (i % 3) == 0 || (i % 5 == 0)){
+                canDivide += 1;
+            }
+        }
+        return num - canDivide;
+    }
 
     /** 3. Write a method called camelCaseMe so that it converts dash/underscore delimited words into camel casing.
      * The first word within the output should be capitalized only if the original word was capitalized
@@ -24,6 +41,24 @@ public class Main {
      "the-stealth-warrior" gets converted to "theStealthWarrior"
      "The_Stealth_Warrior" gets converted to "TheStealthWarrior"
      "The_Stealth-Warrior" gets converted to "TheStealthWarrior" **/
+    public static String camelCaseMe(String str){
+        String result = "";
+        for (int i = 0; i < str.length(); i++){
+            if ((str.charAt(i) == '-') || (str.charAt(i) == '_')){
+                continue;
+            }
+            else if ((i > 0) && (str.charAt(i - 1) == '-')){
+                result += str.substring(i, i+1).toUpperCase();
+            }
+            else if ((i > 0) && (str.charAt(i - 1) == '_')){
+                result += str.substring(i, i+1).toUpperCase();
+            }
+            else{
+                result += str.charAt(i);
+            }
 
+        }
+        return result;
+    }
 
 }
